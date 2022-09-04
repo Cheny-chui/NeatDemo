73f973442e8add9fa6e552d183dad50cadd32982
Cheny <blown.away@qq.com>
2022-09-04 10:48:37 +0800

    move test data to test

--- test/test_loop_free.sh
+++ test/test_loop_free.sh
@@ -1,4 +1,4 @@
-cd $(dirname $0)/../data
-cp loop_free_data-test_shared_links_2.json loop_free_data.json
-cd ..
+cd $(dirname $0)/data/loop_free/
+cp loop_free_data-test_shared_links_2.json $(dirname $0)/../data/loop_free_data.json
+cd $(dirname $0)/../
 python loop_free.py
